bb522110a254441e65dfe1f28d38244f25ca1342
yash-251198 <shetyeyash444@gmail.com>
2025-03-31 02:11:24 +0530

    created a middleware to verify the token in user request that it is correct or not

---
+++ iNoteBook/backend/controller/middleware.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const jwt_sign = process.env.JWT_SECRETE;
+
+const fetchUser = async (req,res,next)=>{
+    try {
+        const token = req.header('auth-token');
+        if(!token) return res.status(400).json({meg:"token is invalid"})
+
+        const data = jwt.verify(token,jwt_sign);
+        console.log(data)
+        req.user = data.user;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({meg:"internal server error"});
+    }
+}
+
+module.exports = {fetchUser};
