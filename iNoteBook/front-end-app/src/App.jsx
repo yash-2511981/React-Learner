afe5597dfde153000241deb92495dc7ed9dab17e
yash-251198 <shetyeyash444@gmail.com>
2025-04-03 20:13:49 +0530

    initialized the front end for iNoteBook and created navbar and Footer

---
+++ iNoteBook/front-end-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./component/Footer";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
